da99c26f5a93b8ea6f0256a9fd90284d6b31488b
ermakovis <tLxPUYg!o#J968>
2022-08-31 15:20:14 +0300

    task #4

--- src/main/java/course/concurrency/m3_shared/immutable/Order.java
+++ src/main/java/course/concurrency/m3_shared/immutable/Order.java
@@ -1,66 +1,93 @@
 package course.concurrency.m3_shared.immutable;
 
+import lombok.Builder;
+
+import java.util.Collections;
 import java.util.List;
 
+import static course.concurrency.m3_shared.immutable.Order.Status.IN_PROGRESS;
 import static course.concurrency.m3_shared.immutable.Order.Status.NEW;
 
-public class Order {
+@Builder
+public final class Order {
 
     public enum Status { NEW, IN_PROGRESS, DELIVERED }
 
-    private Long id;
-    private List<Item> items;
-    private PaymentInfo paymentInfo;
-    private boolean isPacked;
-    private Status status;
+    private final Long id;
+    private final List<Item> items;
+    private final PaymentInfo paymentInfo;
+    private final boolean isPacked;
+    private final Status status;
 
     public Order(List<Item> items) {
-        this.items = items;
+        this.id = null;
+        this.paymentInfo = null;
+        this.isPacked = false;
+        this.items = Collections.unmodifiableList(items);
         this.status = NEW;
     }
 
-    public synchronized boolean checkStatus() {
-        if (items != null && !items.isEmpty() && paymentInfo != null && isPacked) {
-            return true;
-        }
-        return false;
+    public boolean checkStatus() {
+        return !items.isEmpty() && paymentInfo != null && isPacked;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Order withId(Long id) {
+        return Order.builder()
+                .id(id)
+                .items(this.items)
+                .paymentInfo(this.paymentInfo)
+                .isPacked(this.isPacked)
+                .status(this.status)
+                .build();
     }
 
     public List<Item> getItems() {
-        return items;
+        return Collections.unmodifiableList(this.items);
     }
 
     public PaymentInfo getPaymentInfo() {
         return paymentInfo;
     }
 
-    public void setPaymentInfo(PaymentInfo paymentInfo) {
-        this.paymentInfo = paymentInfo;
-        this.status = Status.IN_PROGRESS;
+    public Order withPaymentInfo(PaymentInfo paymentInfo) {
+        return Order.builder()
+                .id(this.id)
+                .items(this.items)
+                .paymentInfo(paymentInfo)
+                .isPacked(this.isPacked)
+                .status(IN_PROGRESS)
+                .build();
     }
 
     public boolean isPacked() {
         return isPacked;
     }
 
-    public void setPacked(boolean packed) {
-        isPacked = packed;
-        this.status = Status.IN_PROGRESS;
+    public Order withPacked(boolean packed) {
+        return Order.builder()
+                .id(this.id)
+                .items(this.items)
+                .paymentInfo(this.paymentInfo)
+                .isPacked(packed)
+                .status(IN_PROGRESS)
+                .build();
     }
 
     public Status getStatus() {
         return status;
     }
 
-    public void setStatus(Status status) {
-        this.status = status;
+    public Order withStatus(Status status) {
+        return Order.builder()
+                .id(this.id)
+                .items(this.items)
+                .paymentInfo(this.paymentInfo)
+                .isPacked(this.isPacked)
+                .status(status)
+                .build();
     }
 }
--- src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
+++ src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
@@ -1,46 +1,64 @@
 package course.concurrency.m3_shared.immutable;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.UnaryOperator;
 
 public class OrderService {
+    private Map<Long, Order> currentOrders = new ConcurrentHashMap<>();
+    private AtomicLong nextId = new AtomicLong(0);
 
-    private Map<Long, Order> currentOrders = new HashMap<>();
-    private long nextId = 0L;
-
-    private synchronized long nextId() {
-        return nextId++;
+    private long nextId() {
+        return nextId.getAndIncrement();
     }
 
-    public synchronized long createOrder(List<Item> items) {
+    public long createOrder(List<Item> items) {
         long id = nextId();
-        Order order = new Order(items);
-        order.setId(id);
+        Order order = Order.builder()
+                .id(id)
+                .items(items)
+                .build();
         currentOrders.put(id, order);
         return id;
     }
 
-    public synchronized void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
-        currentOrders.get(orderId).setPaymentInfo(paymentInfo);
-        if (currentOrders.get(orderId).checkStatus()) {
-            deliver(currentOrders.get(orderId));
-        }
+    public void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
+        updateOrder(orderId, o -> o.withPaymentInfo(paymentInfo));
+        tryToDeliver(orderId);
     }
 
-    public synchronized void setPacked(long orderId) {
-        currentOrders.get(orderId).setPacked(true);
-        if (currentOrders.get(orderId).checkStatus()) {
-            deliver(currentOrders.get(orderId));
-        }
+    public void setPacked(long orderId) {
+        updateOrder(orderId, order -> order.withPacked(true));
+        tryToDeliver(orderId);
     }
 
-    private synchronized void deliver(Order order) {
+    private Order deliver(Order order) {
         /* ... */
-        currentOrders.get(order.getId()).setStatus(Order.Status.DELIVERED);
+        return updateOrder(order.getId(), o -> o.withStatus(Order.Status.DELIVERED));
     }
 
-    public synchronized boolean isDelivered(long orderId) {
+    public boolean isDelivered(long orderId) {
         return currentOrders.get(orderId).getStatus().equals(Order.Status.DELIVERED);
+    }
+
+    private Order updateOrder(long orderId, UnaryOperator<Order> updater) {
+        Order order = currentOrders.get(orderId);
+        Order updatedOrder = updater.apply(order);
+        do {
+            order = currentOrders.get(orderId);
+        } while (currentOrders.replace(orderId, order, updatedOrder));
+        return order;
+    }
+
+    private void tryToDeliver(long orderId) {
+        Order order = currentOrders.get(orderId);
+        if (!order.checkStatus()) return;
+
+        Order deliveredOrder = deliver(order);
+        do {
+            order = currentOrders.get(orderId);
+        } while (currentOrders.replace(orderId, order, deliveredOrder));
     }
 }
