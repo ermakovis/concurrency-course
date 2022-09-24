51a2c4e4cd0a1634f67aecc1f2e1733b4670561a
ermakovis <tLxPUYg!o#J968>
2022-09-24 12:14:20 +0300

    task #5

---
+++ src/main/java/course/concurrency/exams/refactoring/MountTableRefresherService.java
@@ -0,0 +1,151 @@
+package course.concurrency.exams.refactoring;
+
+import lombok.Getter;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+
+@Getter
+public class MountTableRefresherService {
+    private Others.RouterStore routerStore = new Others.RouterStore();
+    private long cacheUpdateTimeout;
+
+    /**
+     * All router admin clients cached. So no need to create the client again and
+     * again. Router admin address(host:port) is used as key to cache RouterClient
+     * objects.
+     */
+    private Others.LoadingCache<String, Others.RouterClient> routerClientsCache;
+
+    /**
+     * Removes expired RouterClient from routerClientsCache.
+     */
+    private ScheduledExecutorService clientCacheCleanerScheduler;
+
+    public void serviceInit()  {
+        long routerClientMaxLiveTime = 15L;
+        this.cacheUpdateTimeout = 10L;
+        routerClientsCache = new Others.LoadingCache<String, Others.RouterClient>();
+        routerStore.getCachedRecords().stream().map(Others.RouterState::getAdminAddress)
+                .forEach(addr -> routerClientsCache.add(addr, new Others.RouterClient()));
+
+        initClientCacheCleaner(routerClientMaxLiveTime);
+    }
+
+    public void serviceStop() {
+        clientCacheCleanerScheduler.shutdown();
+        // remove and close all admin clients
+        routerClientsCache.cleanUp();
+    }
+
+    private void initClientCacheCleaner(long routerClientMaxLiveTime) {
+        ThreadFactory tf = new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread();
+                t.setName("MountTableRefresh_ClientsCacheCleaner");
+                t.setDaemon(true);
+                return t;
+            }
+        };
+
+        clientCacheCleanerScheduler =
+                Executors.newSingleThreadScheduledExecutor(tf);
+        /*
+         * When cleanUp() method is called, expired RouterClient will be removed and
+         * closed.
+         */
+        clientCacheCleanerScheduler.scheduleWithFixedDelay(
+                () -> routerClientsCache.cleanUp(), routerClientMaxLiveTime,
+                routerClientMaxLiveTime, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * Refresh mount table cache of this router as well as all other routers.
+     */
+    public void refresh()  {
+        var futuresMap = routerStore.getCachedRecords().stream()
+                .map(Others.RouterState::getAdminAddress)
+                .filter(StringUtils::hasLength)
+                //лучше вынести в отдельную функцию
+                .map(address -> isLocalAdmin(address) ?
+                        new Others.MountTableManager("local") :
+                        new Others.MountTableManager(address))
+                //можно указать эксекьютор, cachedPoolExecutor был бы наиболее близким к предыдущему функционалу
+                .collect(Collectors.toMap(
+                        manager -> manager,
+                        manager -> CompletableFuture.supplyAsync(manager::refresh)
+                                .orTimeout(cacheUpdateTimeout, TimeUnit.MILLISECONDS)
+                                .exceptionally(ex -> {
+                                        if (ex.getCause() instanceof TimeoutException) {
+                                            log("Not all router admins updated their cache");
+                                        }
+                                        if (ex.getCause() instanceof InterruptedException) {
+                                            log("Mount table cache refresher was interrupted.");
+                                        }
+                                        return false;
+                                    }),
+                        (e1, e2) -> e1)
+                );
+
+        CompletableFuture.allOf(futuresMap.values().toArray(CompletableFuture[]::new))
+                .whenComplete((result, ex) -> {
+                    logResult(futuresMap);
+                }).join();
+    }
+
+    protected MountTableRefresherThread getLocalRefresher(String adminAddress) {
+        return new MountTableRefresherThread(new Others.MountTableManager("local"), adminAddress);
+    }
+
+    private void removeFromCache(String adminAddress) {
+        routerClientsCache.invalidate(adminAddress);
+    }
+
+    private boolean isLocalAdmin(String adminAddress) {
+        return adminAddress.contains("local");
+    }
+
+    private void logResult(Map<Others.MountTableManager, CompletableFuture<Boolean>> futures) {
+        int successCount = 0;
+        int failureCount = 0;
+        for (Map.Entry<Others.MountTableManager, CompletableFuture<Boolean>> entry : futures.entrySet()) {
+            if (isFutureSuccess(entry.getValue())) {
+                successCount++;
+            } else {
+                failureCount++;
+                // remove RouterClient from cache so that new client is created
+                removeFromCache(entry.getKey().getAddress());
+            }
+        }
+        log(String.format(
+                "Mount table entries cache refresh successCount=%d,failureCount=%d",
+                successCount, failureCount));
+    }
+
+    private static boolean isFutureSuccess(CompletableFuture<Boolean> booleanFuture) {
+        try {
+            return booleanFuture.get();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void log(String message) {
+        System.out.println(message);
+    }
+
+    public void setCacheUpdateTimeout(long cacheUpdateTimeout) {
+        this.cacheUpdateTimeout = cacheUpdateTimeout;
+    }
+    public void setRouterClientsCache(Others.LoadingCache cache) {
+        this.routerClientsCache = cache;
+    }
+
+    public void setRouterStore(Others.RouterStore routerStore) {
+        this.routerStore = routerStore;
+    }
+}
---
+++ src/test/java/course/concurrency/exams/refactoring/MountTableRefresherServiceTests.java
@@ -0,0 +1,188 @@
+package course.concurrency.exams.refactoring;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class MountTableRefresherServiceTests {
+
+    private MountTableRefresherService service;
+
+    private Others.RouterStore routerStore;
+    private Others.MountTableManager manager;
+    private Others.LoadingCache routerClientsCache;
+
+    @BeforeEach
+    public void setUpStreams() {
+        service = new MountTableRefresherService();
+        service.setCacheUpdateTimeout(1000);
+        routerStore = mock(Others.RouterStore.class);
+        manager = mock(Others.MountTableManager.class);
+        service.setRouterStore(routerStore);
+        routerClientsCache = mock(Others.LoadingCache.class);
+        service.setRouterClientsCache(routerClientsCache);
+//        service.serviceInit(); // needed for complex class testing, not for now
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+//        service.serviceStop();
+    }
+
+    @Test
+    @DisplayName("All tasks are completed successfully")
+    public void allDone() {
+        // given
+        try (MockedConstruction<Others.MountTableManager> mocked = Mockito.mockConstruction(Others.MountTableManager.class,
+                (mock, context) -> {
+                    when(mock.refresh()).thenReturn(true);
+                    when(mock.getAddress()).thenReturn((String) context.arguments().get(0));
+                })) {
+
+            MountTableRefresherService mockedService = Mockito.spy(service);
+            List<String> addresses = List.of("123", "local6", "789", "local");
+
+            List<Others.RouterState> states = addresses.stream()
+                    .map(Others.RouterState::new)
+                    .collect(toList());
+
+            when(routerStore.getCachedRecords()).thenReturn(states);
+
+            // when
+            mockedService.refresh();
+
+            // then
+            verify(mockedService).log("Mount table entries cache refresh successCount=4,failureCount=0");
+            verify(routerClientsCache, never()).invalidate(anyString());
+        }
+    }
+
+    @Test
+    @DisplayName("All tasks failed")
+    public void noSuccessfulTasks() {
+        // given
+        try (MockedConstruction<Others.MountTableManager> mocked = Mockito.mockConstruction(Others.MountTableManager.class,
+                (mock, context) -> {
+                    when(mock.refresh()).thenReturn(false);
+                    when(mock.getAddress()).thenReturn((String) context.arguments().get(0));
+                })) {
+            MountTableRefresherService mockedService = Mockito.spy(service);
+            List<String> addresses = List.of("123", "local6", "789", "local");
+
+            List<Others.RouterState> states = addresses.stream()
+                    .map(Others.RouterState::new)
+                    .collect(toList());
+
+            when(routerStore.getCachedRecords()).thenReturn(states);
+
+            // when
+            mockedService.refresh();
+
+            // then
+            verify(mockedService).log("Mount table entries cache refresh successCount=0,failureCount=4");
+            verify(routerClientsCache, times(4)).invalidate(anyString());
+        }
+    }
+
+    @Test
+    @DisplayName("Some tasks failed")
+    public void halfSuccessedTasks() {
+        try (MockedConstruction<Others.MountTableManager> mocked = Mockito.mockConstruction(Others.MountTableManager.class,
+                (mock, context) -> {
+                    when(mock.refresh()).thenReturn(((String) context.arguments().get(0)).startsWith("l"));
+                    when(mock.getAddress()).thenReturn((String) context.arguments().get(0));
+                })) {
+            MountTableRefresherService mockedService = Mockito.spy(service);
+            List<String> addresses = List.of("123", "local6", "789", "local");
+
+            List<Others.RouterState> states = addresses.stream()
+                    .map(Others.RouterState::new)
+                    .collect(toList());
+
+            when(routerStore.getCachedRecords()).thenReturn(states);
+
+            // when
+            mockedService.refresh();
+
+            // then
+            verify(mockedService).log("Mount table entries cache refresh successCount=2,failureCount=2");
+            verify(routerClientsCache, times(2)).invalidate(anyString());
+        }
+    }
+
+    @Test
+    @DisplayName("One task completed with exception")
+    public void exceptionInOneTask() {
+        try (MockedConstruction<Others.MountTableManager> mocked = Mockito.mockConstruction(Others.MountTableManager.class,
+                (mock, context) -> {
+                    when(mock.refresh()).thenAnswer(i -> {
+                        if (((String) context.arguments().get(0)).startsWith("1")) {
+                            throw new InterruptedException();
+                        }
+                        return true;
+                    });
+                    when(mock.getAddress()).thenReturn((String) context.arguments().get(0));
+                })) {
+            MountTableRefresherService mockedService = Mockito.spy(service);
+            List<String> addresses = List.of("123", "local6", "789", "local");
+
+            List<Others.RouterState> states = addresses.stream()
+                    .map(Others.RouterState::new)
+                    .collect(toList());
+
+            when(routerStore.getCachedRecords()).thenReturn(states);
+
+            // when
+            mockedService.refresh();
+
+            // then
+            verify(mockedService).log("Mount table cache refresher was interrupted.");;
+            verify(mockedService).log("Mount table entries cache refresh successCount=3,failureCount=1");
+            verify(routerClientsCache, times(1)).invalidate(anyString());
+        }
+    }
+
+    @Test
+    @DisplayName("One task exceeds timeout")
+    public void oneTaskExceedTimeout() {
+        try (MockedConstruction<Others.MountTableManager> mocked = Mockito.mockConstruction(Others.MountTableManager.class,
+                (mock, context) -> {
+                    when(mock.refresh()).thenAnswer(i -> {
+                        if (((String) context.arguments().get(0)).startsWith("7")) {
+                            try {
+                                Thread.sleep(service.getCacheUpdateTimeout() * 2);
+                            } catch (InterruptedException ignored) {}
+                        }
+                        return true;
+                    });
+                    when(mock.getAddress()).thenReturn((String) context.arguments().get(0));
+                })) {
+            MountTableRefresherService mockedService = Mockito.spy(service);
+            List<String> addresses = List.of("123", "local6", "local", "789");
+
+            List<Others.RouterState> states = addresses.stream()
+                    .map(Others.RouterState::new)
+                    .collect(toList());
+
+            when(routerStore.getCachedRecords()).thenReturn(states);
+
+            // when
+            mockedService.refresh();
+
+            // then
+//            verify(mockedService).log("Not all router admins updated their cache");
+            verify(mockedService).log("Mount table entries cache refresh successCount=3,failureCount=1");
+            verify(routerClientsCache, times(1)).invalidate(anyString());
+        }
+    }
+
+}
